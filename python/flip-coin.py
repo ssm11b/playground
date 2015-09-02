fa67d62f94a693f1d663d624518743ca76143679
ssm11b <ss11b@yahoo.com>
2015-09-02 09:12:33 -0700

    Initial commit README and flip-coin.py

---
+++ python/flip-coin.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+
+from random import randint
+
+# This is filp function, it returns heads or tails
+def flip():
+        return 'heads'
+
+print flip()
+
+# randint returns a random number between the range
+print randint(0,10)
+print randint(0,100)
+print randint(0,1000)
+
+
